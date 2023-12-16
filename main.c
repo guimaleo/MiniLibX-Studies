ba3f8ee7f53319b2a17ddd5296f71bc54c344628
guimaleo <leodeguimaraes@gmail.com>
2023-12-16 13:54:42 +0000

    Window Start

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int	main(void)
 {
 	t_win	program;
 
-	program = summon_window(300, 300, "Putain de merde!");
+	program = summon_window(300, 300, "<PROGRAM NAME>");
 	if (!program.mlx_ptr || !program.win_ptr)
 		return (-1);
 	mlx_loop(program.mlx_ptr);
